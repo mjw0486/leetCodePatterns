e23dd1d00376b3b06ff02b7d31fb52f9df30ca42
Michael Wetterauer <mjw0486@gmail.com>
2021-02-07 22:15:07 -0500

    Solves removeLinkedListElements. Time: O(n); Space: O(N);

---
+++ twoPointers/palindromeLinkedList/RemoveLinkedListElements/removeLinkedListElements.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+
+  while (head !== null && head.val === val) {
+    head = head.next;
+  }
+
+  let currentNode : ListNode = head;
+
+  while(currentNode !== null && currentNode.next !== null) {
+    if (currentNode.next.val === val) {
+      currentNode.next = currentNode.next.next;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+  return head;
+};
